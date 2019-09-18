b9c35ea1485e554bc6b83e6b1866444403396d15
Jerome Robbins <jerome.robbins@softbiomed.com>
2019-09-18 12:16:28 -0400

    Initial commit

---
+++ app/src/main/java/com/tregz/mvc/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tregz.mvc.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tregz.mvc.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/tregz/mvc/main/MainLauncher.java
@@ -0,0 +1,23 @@
+package com.tregz.mvc.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class MainLauncher extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 1000);
+    }
+}
